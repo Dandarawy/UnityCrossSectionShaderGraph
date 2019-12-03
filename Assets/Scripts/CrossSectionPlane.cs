cb0cfc91ddec490f588dede6050b40327e8f445d
Dandarawy <abdulla.aldandarawy@gmail.com>
2019-12-03 08:28:39 +0200

    initialize git repo

---
+++ Assets/Scripts/CrossSectionPlane.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[ExecuteInEditMode]
+public class CrossSectionPlane : MonoBehaviour
+{
+    public Material mat1, mat2;
+    public float KeyMoveSpeed = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.forward * KeyMoveSpeed * Time.deltaTime;
+        }
+        else if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= transform.forward * KeyMoveSpeed * Time.deltaTime;
+        }
+        mat1.SetVector("_PlanePosition", transform.position);
+        mat1.SetVector("_PlaneNormal", transform.forward);
+        mat2.SetVector("_PlanePosition", transform.position);
+        mat2.SetVector("_PlaneNormal", transform.forward);
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float Speed;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, Time.deltaTime* Speed);
+    }
+}
